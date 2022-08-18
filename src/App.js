9366ec762db5a22de49f8361af6317543224a2ca
rasfor <nekotanekota@yandex.ru>
2022-08-18 15:25:56 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import MainPageContainer from './features/MainPage/MainPageContainer';
 import Header from './features/Header/Header'
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import NewAnimal from './features/MainPage/NewAnimal/NewAnimal'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <MainPageContainer />
+      <Routes>
+        <Route path='/' element={<MainPageContainer />} />
+        <Route path='/newanimal' element={<NewAnimal />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
